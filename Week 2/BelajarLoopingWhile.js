69cd4ce3d735a1b6083cb9ac70781c3f6e991c0c
KelvinSyahriReza <kelvin30.evin@yahoo.com>
2018-09-05 01:26:27 +0700

    'New'

---
+++ Week 2/BelajarLoopingWhile.js
@@ -0,0 +1,23 @@
+// var flag = 1;
+// while(flag < 10) { // Loop akan terus berjalan selama nilai flag masih dibawah 10
+//   console.log('Iterasi ke-'+flag); // Menampilkan nilai flag pada iterasi tertentu
+//   flag++; // Mengubah nilai flag dengan menambahkan 1 
+// }
+// console.log('Iterasi selesai.')
+
+console.log('Looping Pertama')
+var angka = 2;
+while (angka <= 20 ){
+    
+    console.log(angka + ' - I Love Coding')
+    angka += 2 ;
+}
+console.log ( '<<<<<<<<==================>>>>>>>>>>>>')
+console.log ( 'Looping Yang Kedua')
+
+var angka = 20;
+while (angka > 0 ){
+    
+    console.log(angka + ' - I Will Full Stack Developer')
+    angka -= 2 ;
+}
