438fcdda8e4c31f08dc9cec139bd23daf0dcfc94
KelvinSyahriReza <kelvin30.evin@yahoo.com>
2018-09-05 14:48:16 +0700

    'Update'

---
+++ Week 2/PerulanganGanjilGenap.js
@@ -0,0 +1,10 @@
+var angka = 1
+while (angka <= 100){
+    if (angka % 2 === 0 ){
+        console.log(angka + ' - Genap')
+    } else if (angka % 2 === 1){
+        console.log (angka + ' - Ganjil')
+    }
+    
+    angka ++
+}
